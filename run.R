217a4407e6fdbbee3567437507e4509e34d5e5fa
Brandon Edwards <brandon.edwards144@gmail.com>
2017-12-27 22:42:16 -0500

    Source and call plot functions

--- run.R
+++ run.R
@@ -23,7 +23,14 @@ inputFile <- "MyEBirdData.csv"
 # Import Libraries and Files
 #####################################
 
+# install.packages("ggplot2")
+# install.packages("reshape")
+
+library(ggplot2)
+library(reshape)
+
 source("src/effort-functions.R")
+source("src/plot-recipes.R")
 
 #####################################
 # Read Data
@@ -38,5 +45,8 @@ data$Year <- substr(data$Date,7,10)
 # Replace NA with 0
 data[is.na(data)] <- 0
 
-numChecklists <- getNumChecklists(data)
-
+p <- plotNumChecklists(getNumChecklists(data))
+  
+png("test.png", width = 10.5, height = 4, units = "in", res = 300)
+print(p)
+dev.off()
