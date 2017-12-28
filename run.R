9ef5353f615a8d8bec4b8037e4045d39cd0afa3e
Brandon Edwards <brandon.edwards144@gmail.com>
2017-12-27 19:00:48 -0500

    Source and call function for checklist totals

--- run.R
+++ run.R
@@ -17,16 +17,19 @@ remove(list = ls())
 #####################################
 
 analysisYear <- 2017
+inputFile <- "MyEBirdData.csv"
 
 #####################################
 # Import Libraries and Files
 #####################################
 
+source("src/effort-functions.R")
+
 #####################################
 # Read Data
 #####################################
 
-data <- read.csv("MyEBirdData.csv")
+data <- read.csv(inputFile)
 
 # Add Month and Year field
 data$Month <- substr(data$Date,0,2)
@@ -34,4 +37,6 @@ data$Year <- substr(data$Date,7,10)
 
 # Replace NA with 0
 data[is.na(data)] <- 0
+
+numChecklists <- getNumChecklists(data)
 
