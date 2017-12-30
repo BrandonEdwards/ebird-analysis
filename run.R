5175f492aad00cf35d8764978387d32b251f8148
Brandon Edwards <brandon.edwards144@gmail.com>
2017-12-29 23:00:33 -0500

    Change date format and add cumulative plot call

--- run.R
+++ run.R
@@ -43,6 +43,9 @@ data$Day <- substr(data$Date, 4, 5)
 data$Month <- substr(data$Date, 0, 2)
 data$Year <- substr(data$Date, 7, 10)
 
+# Change date format
+data$Date <- as.Date(paste(data$Year, "-", data$Month, "-", data$Day, sep=""))
+
 # Replace NA with 0
 data[is.na(data)] <- 0
 
@@ -52,14 +55,18 @@ data[is.na(data)] <- 0
 
 # Yearly checklists
 p <- plotYearlyNumChecklists(getNumYearlyChecklists(data))
-  
 png("yearlyNumChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
 print(p)
 dev.off()
 
 # Monthly Checklists for 2017
 p <- plotMonthlyNumChecklists(getNumMonthlyChecklists(data, analysisYear), analysisYear)
-
 png("monthlyNumChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
+print(p)
+dev.off()
+
+# Cumulative Checklists for 2017
+p <- plotCumulativeChecklists(getCumulativeChecklists(data, analysisYear), analysisYear)
+png("monthlyCumulativeChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
 print(p)
 dev.off()
