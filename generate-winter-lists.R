4f66c15b997d4db17854e2630d9f6c01cc9527a4
Brandon Edwards <brandon.edwards144@gmail.com>
2018-01-22 08:09:14 -0500

    Rudimentary winter list generator (only scientific names for now

--- generate-winter-lists.R
+++ generate-winter-lists.R
@@ -46,98 +46,44 @@ dir.create("output/winter-lists")
 
 data <- read.csv(inputFile)
 
-# Add Day, Month, and Year field
-data$Day <- substr(data$Date, 4, 5)
-data$Month <- substr(data$Date, 0, 2)
-data$Year <- substr(data$Date, 7, 10)
+# Fix some date issues and deal with NAs
+data <- cleanData(data)
 
-# Change date format
-data$Date <- as.Date(paste(data$Year, "-", data$Month, "-", data$Day, sep=""))
-
-# Replace NA with 0
-data[is.na(data)] <- 0
+# Remove non-species data
+data <- removeNonSpecies(data)
 
 #####################################
-# Analyze Effort Data
+# Create and Output Winter Lists
 #####################################
 
-# Yearly checklists
-p <- plotYearlyNumChecklists(getNumYearlyChecklists(data))
-png("output/effort/yearlyNumChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
+years <- as.numeric(sort(unique(data$Year)))
 
-# Monthly Checklists for 2017
-p <- plotMonthlyNumChecklists(getNumMonthlyChecklists(data, analysisYear), analysisYear)
-png("output/effort/monthlyNumChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
+for (year in years)
+{
+  # Create empty data frame
+ # winterList <- data.frame(Species = character(),
+#                           Date = character(),
+#                           Location = character(),
+#                           stringsAsFactors = FALSE)
+  
+  winterObs <- data.frame()
+  for (month in c("12", "01", "02"))
+  {
+    if (month == "12")
+    {
+      temp <- data[ which(data$Year == (year - 1)), ]
+    }
+    else
+    {
+      temp <- data[ which(data$Year == year), ]
+    }
+    
+    winterObs <- rbind(winterObs, temp[ which(temp$Month == month), ])
+  }
+  
+  winterList <- as.data.frame(unique(winterObs$Scientific.Name))
+  write.table(winterList, paste("output/winter-lists/", year - 1, "-", year, ".csv", sep=""), row.names = FALSE, col.names = FALSE)
+  
+}
 
-# Cumulative Checklists for 2017
-p <- plotCumulativeChecklists(getCumulativeChecklists(data, analysisYear), analysisYear)
-png("output/effort/monthlyCumulativeChecklists.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
 
-#####################################
-# Analyze Location Data
-#####################################
-
-# Total Yearly Counties
-p <- plotYearlyNumCounties(getYearlyTotalCounties(data))
-png("output/location/yearlyNumCounties.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-# Top 10 Counties
-p <- plotTopTenCounties(getTopTenCounties(data, analysisYear), analysisYear)
-png("output/location/topTenCounties.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-#####################################
-# Analyze Total Species Data
-#####################################
-
-#From here on in, we will want data only at the species level
-data.sp <- removeNonSpecies(data)
-
-# Total Yearly Species
-p <- plotYearlyTotalSpecies(getNumYearlySpecies(data.sp))
-png("output/total/yearlyNumSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-# Total Monthly Species
-p <- plotMonthlyTotalSpecies(getNumMonthlySpecies(data.sp, analysisYear), analysisYear)
-png("output/total/monthlyNumSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-# Cumulative Year Species
-p <- plotCumulativeYearSpecies(getCumulativeSpecies(data.sp, analysisYear), analysisYear)
-png("output/total/cumulativeSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-#####################################
-# Analyze Life Birds Data
-#####################################
-
-# Cumulative Life Species
-p <- plotCumulativeLifeSpecies(getCumulativeLifeSpecies(data.sp, analysisYear), analysisYear)
-png("output/life/cumulativeLifeSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-#Yearly Life Species
-p <- plotYearlyLifeSpecies(getYearlyLifeSpecies(data.sp))
-png("output/life/yearlyLifeSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
-
-#Yearly Available Life Species
-p <- plotYearlyAvailableLifeSpecies(getYearlyAvailableLifeSpecies(data.sp))
-png("output/life/yearlyAvailableLifeSpecies.png", width = 10.5, height = 4, units = "in", res = 300)
-print(p)
-dev.off()
